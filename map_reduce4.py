7da9472d7f3daa148669011ee08c447ff77bdb91
hbelghachem <hbelghachem@example.com>
2026-04-09 17:35:23 +0200

    Add files from TRUC

---
+++ map_reduce4.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class TagsPerMovie(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_tags,
+                   reducer=self.reducer_count_tags)
+        ]
+
+    def mapper_get_tags(self, _, line):
+        parts = line.split(',')
+        
+        if len(parts) == 4 and parts[0] != 'userId':
+            userId, movieId, tag, timestamp = parts
+            
+            # Clé = (film, utilisateur)
+            yield (movieId, userId), 1
+
+    def reducer_count_tags(self, key, values):
+        # On compte combien de tags cet utilisateur a mis pour ce film
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    TagsPerMovie.run()
